b69b84ef515395b43f0e9d503a50b132d9c6ed8e
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2024-06-05 11:54:27 -0600

    Create getHumanChoice() function
    Function will prompt user to type "rock", "paper", or "scissors". If
    user clicks "cancel", the user will be prompted with the question to end
    the game. If they click ok, the function is returned, otherwise they
    will return to the original prompt. The function then confirms a valid
    input and then returns that input.

--- script.js
+++ script.js
@@ -13,3 +13,59 @@ function getComputerChoice() {
     }
     return computerChoice;
 }
+
+function getHumanChoice() {
+    // let answer = prompt("Rock, Paper, or Scissors\nChoose one by typing it below:").toLowerCase();
+    // let humanChoice;
+
+    // if (answer === "rock") {
+    //     humanChoice = "rock";
+    // } else if (answer === "paper") {
+    //     humanChoice === "paper";
+    // } else if (answer === "scissors") {
+    //     humanChoice === "scissors";
+    // } else {
+    //     prompt("Invalid input:\nPlease enter \"rock\", \"paper\", or \"scissors\")
+    // }
+
+    let answer = null;
+    let humanChoice;
+    let invalidInput = true;
+
+    while (invalidInput) {
+        while (answer === null) {
+            answer = prompt("Rock, Paper, or Scissors\nChoose one by typing it below:");
+            
+            // If user clicks cancel
+            if (answer === null) {
+                let cancel = confirm("End game?");
+                
+                if (cancel === true) {
+                    console.log("Game ended. Refresh page to restart game.")
+                    return;
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        answer = answer.toLowerCase();
+
+        if (answer === "rock") {
+            humanChoice = "rock";
+            invalidInput = false;
+        } else if (answer === "paper") {
+            humanChoice = "paper";
+            invalidInput = false;
+        } else if (answer === "scissors") {
+            humanChoice = "scissors";
+            invalidInput = false;
+        } else {
+            alert("Invalid input")
+        }
+    }
+
+    return humanChoice;
+}
+let humanChoice = getHumanChoice();
+console.log("You picked: " + humanChoice);
