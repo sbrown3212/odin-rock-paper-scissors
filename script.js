4e1f79aea61e755c36b222b8c6d7819f5934e208
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2024-06-05 10:52:06 -0600

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world!")
+console.log("Rock, Paper, Scissors")
+
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random() * 3);
+    let computerChoice;
+
+    if (randNum === 0) {
+        computerChoice = "rock";
+    } else if (randNum === 1) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
+}
